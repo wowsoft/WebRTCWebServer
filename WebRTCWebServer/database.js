405f1343b371805836295294dc64e344acf17659
yhkim <yhkimjjang@gmail.com>
2015-06-05 11:52:35 +0900

    커밋

--- WebRTCWebServer/database.js
+++ WebRTCWebServer/database.js
@@ -5,9 +5,31 @@ var pool = mysql.createPool({
     host: "localhost",
     user: "yhkim",
     password: "1111",
-    database: "sampeldb",
+    database: "webrtcdb",
     connectionLimit: 10
 });
+
+exports.insertUser = function (obj, callback) {
+    var sql = "insert into user1 (user_email, user_password, user_year, user_sex, user_gcm_register_id) values (?, ?, ?, ?, ?)";
+
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            logger.error(err.stack);
+            callback(true, "connection error");
+            return;
+        }
+        connection.query(sql, [obj.email, obj.password, obj.year, obj.sex, obj.gcm_register_id], function (err, results) {
+            connection.release();
+            if (err) {
+                logger.error(err.stack);
+                callback(true, "insertUser query error");
+                return;
+            }
+            
+            callback(false, null, results);
+        });
+    });
+};
 
 exports.getUserInfo = function (obj, callback) {
     var sql = "SELECT title, content1 FROM entries WHERE title = ?";
--- WebRTCWebServer/routes/user.js
+++ WebRTCWebServer/routes/user.js
@@ -3,7 +3,11 @@ var router = express.Router();
 var db = require("../database");
 
 var returnObj = function (err, msg, rows) {
-    if (err) { return { result: "ERROR", err: msg }; }
+    if (err) {
+        return {
+            result: "ERROR"
+        };
+    }
 
     return {
         result: "OK",
@@ -17,13 +21,16 @@ router.get("/", function (req, res) {
     res.json({ user: "tobi" });
 });
 
-router.get("/add", function (req, res){
+router.post("/add", function (req, res){
     param = req.body;
-    //console.log(param);
-    db.getUserInfo(param, function (err, msg, rows) {
+    db.insertUser(param, function (err, msg, rows) {
         res.header("Content-Type", "application/json");
 
-        if (err) { res.send(500, returnObj(err, msg)); return; }
+        if (err) {
+            console.log("error: " + msg);
+            res.send(500, returnObj(err, msg));
+            return;
+        }
 
         res.send(200, returnObj(err, msg, rows));
     });
